62c54a18d8d6b9330d30f325b1272fc9c3b5a284
陈淼 <chenmiao@chenmiaodemacbook-pro.local>
2016-03-30 12:58:21 +0800

    Initial Commit

---
+++ MyBiliBili/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyBiliBili
+//
+//  Created by 陈淼 on 16/3/30.
+//  Copyright © 2016年 陈淼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyBiliBili/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyBiliBili
+//
+//  Created by 陈淼 on 16/3/30.
+//  Copyright © 2016年 陈淼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
